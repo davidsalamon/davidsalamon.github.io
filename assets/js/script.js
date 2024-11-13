2e78c13936119819c67f157bc059fd5270d86a46
davidsalamon <salamon.david@gmail.com>
2024-11-13 10:45:25 +0000

    f

--- assets/js/script.js
+++ assets/js/script.js
@@ -205,6 +205,3 @@
 
 
 })(jQuery);
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
